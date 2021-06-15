c6de9dcd19cf84dfa0e1266f5f875e41694521c2
rachelazar <r0556782231@gmail.com>
2021-06-16 01:20:43 +0300

    files

--- angular/Dreamlike/src/app/app.module.ts
+++ angular/Dreamlike/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdultUserComponent } from './adult-user/adult-user.component';
-import { HomeComponent } from './shared/home/home.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './manager/components/login/login.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { ProductComponent } from './shared/components/product/product.component';
+import { SelectedProductComponent } from './user/components/selected-product/selected-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdultUserComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    ProductComponent,
+    SelectedProductComponent
   ],
   imports: [
     BrowserModule,
--- angular/Dreamlike/src/app/user/user.module.ts
+++ angular/Dreamlike/src/app/user/user.module.ts
@@ -9,4 +9,4 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class YoungUserModule { }
+export class UserModule { }
